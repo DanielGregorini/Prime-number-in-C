b760f76d7365dd197f95a0cab8b8684fb49e24df
DanielGregorini <danielgregorini@alunos.utfpr.edu.br>
2021-10-18 22:16:31 -0300

    Create 21-quantos numeros primos em um periodo.c

---
+++ 21-quantos numeros primos em um periodo.c
@@ -0,0 +1,37 @@
+  #include <stdio.h>
+  #include <stdlib.h>
+  #include <stdbool.h>
+  #include <locale.h> 
+  #include <time.h> 
+  #include <math.h>
+
+  int main(void) {
+
+   int i, n, valor, aux, nprimo, nprimosfinal;
+   aux = 0;
+   valor = 0;
+   nprimosfinal = 0;
+
+   printf("Digite um valor: ");
+   scanf("%d", &nprimo);
+
+    for (n = 1; n <= nprimo; n++){ 
+     aux = 0;
+      for (i = 1; i <= n; i++){
+
+        if (n % i == 0){
+          aux++;
+        }
+
+      }
+      if (aux <= 2){
+        printf("%d is a prime number\n",n);
+        nprimosfinal++;
+      }
+
+    }
+   printf("-----------------------------------------\n");
+   printf("Is %d prime number in between 1 and %d",nprimosfinal,nprimo);
+
+  return 0;
+  }
